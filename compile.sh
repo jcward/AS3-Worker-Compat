dad04d2d5cc5ae0d1f05926661db1a73992b4635
Jeff Ward <jeff.ward@gmail.com>
2012-11-13 15:41:07 -0700

    Removed unnecessary -target-player
    Was testing compiling with static Worker imports

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-mxmlc com/lilcodemonkey/workers/WorkerCompatTest.as -o WorkerCompatTest.swf -source-path+=. -static-link-runtime-shared-libraries -swf-version=17 -target-player=11.5
+mxmlc com/lilcodemonkey/workers/WorkerCompatTest.as -o WorkerCompatTest.swf -source-path+=. -static-link-runtime-shared-libraries -swf-version=17
